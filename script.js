50554e8992b7f5f55650075a418ba1143055434c
Yumacorn <ReachRyanMui@gmail.com>
2021-04-17 18:19:23 -0400

    updated buttons to update DOM with coffee and person statuses

---
+++ script.js
@@ -0,0 +1,23 @@
+let heartMug = document.getElementById('cup');
+let personStatus = document.getElementById('human');
+
+function buttonClick(event) {
+    switch(event.target.id){
+        case 'pour':
+            alert('Pour the coffee');
+            heartMug.textContent = 'Cup: FULL';
+            break;
+        case 'drink':
+            alert('Drink the coffee');
+            heartMug.textContent = 'Cup: Empty';
+            personStatus.textContent = 'Person: Satisfied!';
+            break;
+    }
+    
+}
+
+const buttons = document.querySelectorAll('button');
+
+for(let i = 0; i < buttons.length; i++) {
+    buttons[i].addEventListener('click', buttonClick);
+}
